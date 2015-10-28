75d451376fe954daf85370735b9c810de48f342c
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-10-27 23:36:37 -0400

    created User/Conversation classes

---
+++ BlocTalk/BlocTalk/Conversation.m
@@ -0,0 +1,13 @@
+//
+//  Conversation.m
+//  BlocTalk
+//
+//  Created by Robert von Stange on 10/27/15.
+//  Copyright (c) 2015 Robert von Stange. All rights reserved.
+//
+
+#import "Conversation.h"
+
+@implementation Conversation
+
+@end
